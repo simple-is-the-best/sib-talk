726291b24a03e4eb10799f8b82d2146fa2c05de3
nopecho <ch.jooon@gmail.com>
2023-01-23 20:00:48 +0900

    chore : gradle setting

---
+++ boot/talk-message/src/test/java/com/sib/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sib.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
